ac9df1d4d240dfaee4343f6fd44ca78b9f0b359c
WefNET <john.wefler@gmail.com>
2018-05-28 10:20:16 -0500

    Oh boy!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes, RouterLink } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RosterComponent } from './roster/roster.component';
+import { RecruitingComponent } from './recruiting/recruiting.component';
+import { SaleComponent } from './sale/sale.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+import { TabViewModule } from 'primeng/tabview';
+import { AccordionModule } from 'primeng/accordion';
+import { CardModule } from 'primeng/card';
+
+
+const routes: Routes = [
+  { path: '*', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'roster', component: RosterComponent },
+  { path: 'joinnow', component: RecruitingComponent },
+  { path: 'recipes', component: RecipesComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RosterComponent,
+    HomeComponent,
+    RecruitingComponent,
+    SaleComponent,
+    RecipesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    TabViewModule,
+    AccordionModule,
+    CardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
